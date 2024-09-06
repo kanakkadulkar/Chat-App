67bdce7ce685aaff1893ad816888feaffe05ab0f
kanakkadulkar <144000040+kanakkadulkar@users.noreply.github.com>
2024-09-06 23:56:43 +0530

    logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import Chat from "./components/chat/Chat.jsx";
 import List from "./components/list/List.jsx";
 import Detail from "./components/detail/detail.jsx";
@@ -10,41 +9,39 @@ import { auth } from "./lib/firebase.js";
 import { useUserStore } from "./lib/userStore.js";
 
 const App = () => {
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
 
-  const {currentUser,isLoading,fetchUserInfo}=useUserStore()
-
-
-  useEffect(()=>{
-     const unSub=onAuthStateChanged(auth,(user)=>{
+  useEffect(() => {
+    console.log("useEffect called");
+    const unSub = onAuthStateChanged(auth, (user) => {
+      console.log("onAuthStateChanged callback called");
       fetchUserInfo(user?.uid);
     });
 
-      return()=>{
-        unSub();
-      }
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
 
-  },[fetchUserInfo]);
-  
-  if(isLoading) return <div className="loading">Loading...</div>
+  console.log("isLoading:", isLoading);
+  console.log("currentUser:", currentUser);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
 
   return (
-    <div className='container'>
-      {
-        currentUser?( <>
-          <List/>
-          <Chat/>
-          <Detail/>
-          </>):(
-            <Login/>
-          )
-      }
-   <Notification/>
-    
-    
+    <div className="container">
+      {currentUser ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
+      <Notification />
     </div>
-
-    
   );
 };
 
-export default App
+export default App;
