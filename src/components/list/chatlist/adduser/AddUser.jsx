cfc8cd7f44d434a1ca4363656e533a0f09ace328
kanakkadulkar <144000040+kanakkadulkar@users.noreply.github.com>
2024-09-15 23:31:58 +0530

    2.18 incomplete

--- src/components/list/chatlist/adduser/AddUser.jsx
+++ src/components/list/chatlist/adduser/AddUser.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './addUser.css'
-import { collection, getDocs, query, where } from 'firebase/firestore'
+import { collection, doc, getDocs, query, serverTimestamp, setDoc, where } from 'firebase/firestore'
 import { db } from '../../../../lib/firebase'
 
 
@@ -9,26 +9,43 @@ import { db } from '../../../../lib/firebase'
    const [user, setUser] =useState(null);
 
  const handleSearch=async (e)=>{
-   e.preventdefault()
-   const formData = new FormData(e.target)
+   e.preventDefault()
+   const formData = new FormData(e.target);
    const username=formData.get("username");
 
    try{
-      const userRef=collection(db,"users");
-
-      const q=query(userRef,where("username","==",username));
       
+      const userRef=collection(db,"users");
+      const q=query(userRef,where("username","==",username));   
       const querySnapShot=await getDocs(q);
-
       if(!querySnapShot.empty) {
-         setUser(querySnapShot.doc(0).data());
+         setUser(querySnapShot.docs[0].data());
+         console.log("nooo");
       }
 
    }catch(err){
       console.error(err)
    }
+}
+const handleAdd=async()=>{
+   const chatRef=collection(db,"chats");
+   const userchatsRef=collection(db,"userchats");
 
- }
+   try{
+      const newChatRef=doc(chatRef)
+
+
+         await setDoc(newChatRef,{
+            createdAt:serverTimestamp(),
+            messages:[],
+         })
+ console.log(newChatRef.id)
+
+   }catch(err){
+      console.log(err)
+   }
+} 
+ 
   return (
     <div className='addUser'>
        <form onSubmit={handleSearch}>
@@ -42,7 +59,7 @@ import { db } from '../../../../lib/firebase'
                 {user.username}
             </span>
           </div>
-          <button>Add User</button>
+          <button onClick={handleAdd}>Add User</button>
        </div>}
     </div>
   )
